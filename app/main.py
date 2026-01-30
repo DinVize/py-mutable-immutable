dca890858d2068a19d0debce311d03d42400770c
Eduard <kondratriukeduard@gmail.com>
2026-01-30 23:44:18 +0200

    add sorted dictionery

--- app/main.py
+++ app/main.py
@@ -25,3 +25,17 @@ my_favourite_films.append("Hunert")
 profile_info = ("michel", "michel@gmail.com", "12345678", "Ukraine")
 marks["Ryan"] = 2
 collection_of_coins.add(14)
+sorted_variables = {
+    "mutable": [
+        my_favourite_films,
+        marks,
+        collection_of_coins,
+    ],
+    "immutable": [
+        lucky_number,
+        pi,
+        one_is_a_prime_number,
+        name,
+        profile_info,
+    ],
+}
